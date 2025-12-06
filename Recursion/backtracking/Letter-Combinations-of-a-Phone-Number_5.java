106ea42cd3ce91c09922bb5d45fcd232e1dfbef9
Mohit555000 <gangwanimohit42@gmail.com>
2025-12-06 20:53:55 +0530

    Update Letter-Combinations-of-a-Phone-Number_5.java

--- Recursion/backtracking/Letter-Combinations-of-a-Phone-Number_5.java
+++ Recursion/backtracking/Letter-Combinations-of-a-Phone-Number_5.java
@@ -1,6 +1,6 @@
 //https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/?envType=problem-list-v2&envId=backtracking
 //https://www.youtube.com/watch?v=tWnHbSHwNmA
-
+//method 1
 class Solution {
     public void solve(String digits,String output,int index,List<String> ans,String[] mapping){
         if(index>=digits.length()){
@@ -25,5 +25,46 @@ class Solution {
         solve(digits,output,index,ans,mapping);
         return ans;
 
+    }
+}
+
+
+//method 2
+class Solution {
+
+    public void solve(String digits, StringBuilder output, int index, List<String> ans, String[] mapping) {
+
+        if (index == digits.length()) {
+            ans.add(output.toString());
+            return;
+        }
+
+        int number = digits.charAt(index) - '0';
+        char[] letters = mapping[number].toCharArray();
+
+        for (char ch : letters) {
+
+            // add character
+            output.append(ch);
+
+            // recursive call
+            solve(digits, output, index + 1, ans, mapping);
+
+            // BACKTRACK: remove last added character
+            output.deleteCharAt(output.length() - 1);
+        }
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        if (digits.length() == 0) return ans;
+
+        String[] mapping = {
+            "", "", "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        solve(digits, new StringBuilder(), 0, ans, mapping);
+        return ans;
     }
 }

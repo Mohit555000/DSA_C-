7d8b41729b86935a34616ba68939873f7212f209
Mohit555000 <gangwanimohit42@gmail.com>
2026-02-26 19:25:18 +0530

    Update 8_Flattening_a_Linked_List.java

--- Linkedlist/medium_problems/8_Flattening_a_Linked_List.java
+++ Linkedlist/medium_problems/8_Flattening_a_Linked_List.java
@@ -43,3 +43,59 @@ class Solution {
         return temp;
     }
 }
+
+In brute force approach we are sorting and using an extra space.
+In Optimized approach we can remove sorting (from the child nodes) since it is already sorted and we won't avoid the use of external array.
+
+Optimized Approach
+    /*
+class Node {
+    int data;
+    Node next;
+    Node bottom;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        bottom = null;
+    }
+}
+*/
+class Solution {
+    public Node merge(Node a,Node b){
+        Node dummy=new Node(0);
+        Node temp=dummy;
+        
+        while(a!=null && b!=null){
+            if(a.data<b.data){
+                temp.bottom=a;
+                temp=a;
+                a=a.bottom;
+            }
+            else{
+                temp.bottom=b;
+                temp=b;
+                b=b.bottom;
+            }
+            if(a!=null){
+                temp.bottom=a;
+            }
+            else{
+                temp.bottom=b;
+            }
+            
+        }
+        return dummy.bottom;
+    }
+    public Node flatten(Node root) {
+        // code here
+        if(root==null || root.next==null){
+            return root;
+        }
+        
+        root.next=flatten(root.next);
+        
+        root=merge(root,root.next);
+        return root;
+    }
+}

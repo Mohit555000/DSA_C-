969fa09214a8702091e868ee327ffd5e9dbdba1d
Mohit555000 <gangwanimohit42@gmail.com>
2026-04-15 15:48:21 +0530

    Update 4_Sum_of_Subarray_Minimums.java

--- Stacks and Queues/Questions/4_Sum_of_Subarray_Minimums.java
+++ Stacks and Queues/Questions/4_Sum_of_Subarray_Minimums.java
@@ -1,4 +1,4 @@
-//Method 1 (Brute Force Approach)
+//Method 1 (Brute Force Approach) (good)
 // formed all the subarrays and used min heap to fetch the min element from each of the sub array 
 // It will give TLE
 class Solution {
@@ -19,4 +19,19 @@ class Solution {
 }
 
 
-//Method 2 (Optimized Approach)
+//Method 2 (Optimized Approach) (better)
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n =arr.length;
+        int result=0;
+        int modulo=1000000007;
+        for(int i=0;i<n;i++){
+            int min=arr[i];
+            for(int j=i;j<n;j++){
+                min=Math.min(min,arr[j]);
+                result=(result+min)%modulo;
+            }
+        }
+        return result;
+    }
+}

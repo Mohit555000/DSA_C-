e04fe9ad3973d114e6e449891ad4e5814d0027f2
Mohit555000 <gangwanimohit42@gmail.com>
2026-05-18 23:11:38 +0530

    Create 37_Kth largest element in BST.java

---
+++ trees/37_Kth largest element in BST.java
@@ -0,0 +1,80 @@
+//https://www.geeksforgeeks.org/problems/kth-largest-element-in-bst/1
+
+//brute force approach
+/*
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Solution {
+    // return the Kth largest element in the given BST rooted at 'root'
+    public void inorder(Node root,ArrayList<Integer>list){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,list);
+        list.add(root.data);
+        inorder(root.right,list);
+    }
+    public int kthLargest(Node root, int k) {
+        // Your code here
+        if(root==null){
+            return -1;
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        inorder(root,list);
+        Collections.sort(list);
+        return list.get(list.size()-k);
+    }
+}
+
+
+//Optimized Approach
+/*
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Solution {
+    int ans=-1;
+    int count=0;
+    // return the Kth largest element in the given BST rooted at 'root'
+
+    public void inorder(Node root,int k){
+        if(root==null){
+            return;
+        }
+        inorder(root.right,k);
+        count++;
+        if(count==k){
+            ans=root.data;
+        }
+        inorder(root.left,k);
+    }
+    public int kthLargest(Node root, int k) {
+        // Your code here
+        if(root==null){
+            return -1;
+        }
+        inorder(root,k);
+        return ans;
+    }
+}

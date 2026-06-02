5e15e60f0c691d6db4e8303a8ba5c75e570fb8fb
Mohit555000 <gangwanimohit42@gmail.com>
2026-06-02 12:36:13 +0530

    Update 9_K-th Largest Sum Contiguous Subarray.java

--- heaps/9_K-th Largest Sum Contiguous Subarray.java
+++ heaps/9_K-th Largest Sum Contiguous Subarray.java
@@ -1,7 +1,7 @@
 //https://www.geeksforgeeks.org/problems/k-th-largest-sum-contiguous-subarray/1
 //https://www.youtube.com/watch?v=eccAKrmffh8&list=PLWlla5Y-1FqyOqTat11CmcQp1nl079d0W&index=3
 
-Brute Force Approach TC O(n^2 log(n))
+Brute Force Approach TC O(n^2 log(n)) and space complexity is O(n^2)
   class Solution {
     public static int kthLargest(int[] arr, int k) {
         // code here
@@ -17,5 +17,29 @@ Brute Force Approach TC O(n^2 log(n))
         return list.get(k-1);
     }
 }
-Optimized is remaining
+Optimized is TC O(n^2 log (k) and space complexity is O(k)
+  class Solution {
+    public static int kthLargest(int[] arr, int k) {
+        // code here
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum=sum+arr[j];
+                if(pq.size()<k){
+                    pq.add(sum);
+                }
+                else{
+                    if(sum>pq.peek()){
+                        pq.poll();
+                        pq.add(sum);
+                    }
+                }
+            }
+        }
+        return pq.poll();
+        
+    }
+}
+
   

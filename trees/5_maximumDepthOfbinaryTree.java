fd00426efdc7ac6413cae9a998bef985d2475592
Mohit555000 <gangwanimohit42@gmail.com>
2026-06-02 15:01:19 +0530

    Update 5_maximumDepthOfbinaryTree.java

--- trees/5_maximumDepthOfbinaryTree.java
+++ trees/5_maximumDepthOfbinaryTree.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -24,3 +25,5 @@ class Solution {
     }
 
 }
+
+Complexity AnalysisTime Complexity: $O(N)$, where $N$ is the number of nodes in the tree. The algorithm has to visit every single node exactly once to find the maximum depth.Space Complexity: * Worst Case: $O(N)$ if the tree is completely unbalanced (skewed like a linked list), meaning the call stack will hold all nodes.Best/Average Case: $O(\log N)$ if the tree is balanced, because the maximum height of the call stack matches the height of the tree.

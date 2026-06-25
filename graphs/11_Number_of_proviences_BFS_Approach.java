e85311782590813dcbbbb18525bd90e0a9f057a7
Mohit555000 <gangwanimohit42@gmail.com>
2026-06-25 14:54:37 +0530

    Create 11_Number_of_proviences_BFS_Approach.java

---
+++ graphs/11_Number_of_proviences_BFS_Approach.java
@@ -0,0 +1,45 @@
+//https://leetcode.com/problems/number-of-provinces/
+//https://www.youtube.com/watch?v=YDMkSvNdB20&list=PLpIkg8OmuX-LZB9jYzbbZchk277H5CbdY&index=11
+
+class Solution {
+    public void BFS(int start,Map<Integer,List<Integer>> adj,boolean[] visited){
+        Queue<Integer> q=new LinkedList<>();
+        visited[start]=true;
+        q.add(start);
+        while(!q.isEmpty()){
+            int currentElement=q.poll();
+            for(int neighbour:adj.get(start)){
+                if(visited[neighbour]==false){
+                    BFS(neighbour,adj,visited);
+                }
+            }
+        }
+
+    }
+    public int findCircleNum(int[][] isConnected) {
+        Map<Integer,List<Integer>> adj=new HashMap<>();
+        int m=isConnected.length;
+        int n=isConnected[0].length;
+        // Create Map
+        for(int i=0;i<m;i++){
+            adj.put(i,new ArrayList<>());
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j]==1){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
+        int count=0;
+        boolean[] visited=new boolean[m];
+        for(int i=0;i<m;i++){
+            if(visited[i]==false){
+                BFS(i,adj,visited);
+                count++;
+            }
+        }
+        return count;
+    }
+}

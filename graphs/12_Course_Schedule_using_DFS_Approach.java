92bcfcb6a682a0234049c985775e6149409d3a66
Mohit555000 <gangwanimohit42@gmail.com>
2026-06-25 16:06:07 +0530

    Update 12_Course_Schedule_using_DFS_Approach.java

--- graphs/12_Course_Schedule_using_DFS_Approach.java
+++ graphs/12_Course_Schedule_using_DFS_Approach.java
@@ -1,44 +1,44 @@
 // https://leetcode.com/problems/course-schedule/description/
 // https://www.youtube.com/watch?v=lqjlGGMjSMU&list=PLpIkg8OmuX-LZB9jYzbbZchk277H5CbdY&index=12   ismai khan's algorithm use kiya hai 
 class Solution {
-    public void BFS(int start,Map<Integer,List<Integer>> adj,boolean[] visited){
-        Queue<Integer> q=new LinkedList<>();
+    public boolean DFS(int start,Map<Integer,List<Integer>> adj,boolean[] visited,boolean[] currentRecursion){
         visited[start]=true;
-        q.add(start);
-        while(!q.isEmpty()){
-            int currentElement=q.poll();
-            for(int neighbour:adj.get(start)){
-                if(visited[neighbour]==false){
-                    BFS(neighbour,adj,visited);
+        currentRecursion[start]=true;
+
+        for(int neighbour:adj.get(start)){
+            if(visited[neighbour]==false){
+                if(DFS(neighbour,adj,visited,currentRecursion)==true){
+                    return true;
                 }
+            } else if(currentRecursion[neighbour]==true){
+                return true;
             }
         }
-
+        currentRecursion[start]=false;
+        return false;
     }
-    public int findCircleNum(int[][] isConnected) {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //make the adjacencey list
         Map<Integer,List<Integer>> adj=new HashMap<>();
-        int m=isConnected.length;
-        int n=isConnected[0].length;
-        // Create Map
-        for(int i=0;i<m;i++){
+        for(int i=0;i<numCourses;i++){
             adj.put(i,new ArrayList<>());
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && isConnected[i][j]==1){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
+        for(int[] prerequisite:prerequisites){
+            int u=prerequisite[1];
+            int v=prerequisite[0];
+
+            adj.get(u).add(v); //since it is a directed graph
+        }
+        boolean[] visited=new boolean[numCourses];
+        boolean[] currentRecursion=new boolean[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            if(visited[i]==false){
+                if(DFS(i,adj,visited,currentRecursion)==true){
+                    return false;
                 }
             }
         }
-        int count=0;
-        boolean[] visited=new boolean[m];
-        for(int i=0;i<m;i++){
-            if(visited[i]==false){
-                BFS(i,adj,visited);
-                count++;
-            }
-        }
-        return count;
+        return true;
     }
 }

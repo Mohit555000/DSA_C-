7e2984ed694df22cffe95bd2a4069e283ddbf49d
Mohit555000 <gangwanimohit42@gmail.com>
2026-07-26 18:01:09 +0530

    Update 22_dijkstra's_algorithm_using_priority_queue.java

--- graphs/22_dijkstra's_algorithm_using_priority_queue.java
+++ graphs/22_dijkstra's_algorithm_using_priority_queue.java
@@ -1,80 +1,66 @@
 //https://www.youtube.com/watch?v=xQ3vjWwFRuI&list=PLpIkg8OmuX-LZB9jYzbbZchk277H5CbdY&index=24
 //https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
 class Solution {
-
-    class Pair {
+    class Pair{
         int node;
         int weight;
-
-        Pair(int node, int weight) {
-            this.node = node;
-            this.weight = weight;
+        
+        public Pair(int node,int weight){
+            this.node=node;
+            this.weight=weight;
         }
     }
-
-    public int[] dijkstra(int V, int[][] edges, int src) {
-
-        Map<Integer, List<Pair>> adj = buildGraph(edges);
-
-        return findShortestDistance(V, adj, src);
-    }
-
-    // Build Adjacency List
-    private Map<Integer, List<Pair>> buildGraph(int[][] edges) {
-
+    public Map<Integer,List<Pair>> buildGraph(int[][] edges,int V){
         Map<Integer, List<Pair>> adj = new HashMap<>();
 
-        for (int[] edge : edges) {
+        for (int i = 0; i < V; i++) {
+            adj.put(i, new ArrayList<>());
+        }
 
+        for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
             int w = edge[2];
 
-            adj.putIfAbsent(u, new ArrayList<>());
-            adj.putIfAbsent(v, new ArrayList<>());
-
             adj.get(u).add(new Pair(v, w));
-            adj.get(v).add(new Pair(u, w)); // Undirected Graph
+            adj.get(v).add(new Pair(u, w));
         }
-
         return adj;
     }
-
-    // Dijkstra's Algorithm
-    private int[] findShortestDistance(int V, Map<Integer, List<Pair>> adj, int src) {
-
-        int[] dist = new int[V];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-
-        dist[src] = 0;
-        pq.offer(new Pair(src, 0));
-
-        while (!pq.isEmpty()) {
-
-            Pair current = pq.poll();
-
-            int node = current.node;
-            int currDist = current.weight;
-
-            if (currDist > dist[node]) {
+    public int[] findShortestDistamce(int V,Map<Integer,List<Pair>> adj,int src){
+        int[] distance=new int[V];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+        
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.weight-b.weight);
+        distance[src]=0;
+        pq.offer(new Pair(src,0));
+        
+        while(!pq.isEmpty()){
+            Pair current=pq.poll();
+            int node=current.node;
+            int currentDistance=current.weight;
+            
+            if(currentDistance>distance[node]){
                 continue;
             }
-
-            for (Pair neighbour : adj.getOrDefault(node, new ArrayList<>())) {
-
-                int nextNode = neighbour.node;
-                int edgeWeight = neighbour.weight;
-
-                if (currDist + edgeWeight < dist[nextNode]) {
-
-                    dist[nextNode] = currDist + edgeWeight;
-                    pq.offer(new Pair(nextNode, dist[nextNode]));
+            for(Pair neighbour:adj.get(node)){
+                int nextNode=neighbour.node;
+                int edgeWidth=neighbour.weight;
+                
+                if(currentDistance+edgeWidth<distance[nextNode]){
+                    distance[nextNode]=currentDistance+edgeWidth;
+                    pq.offer(new Pair(nextNode,distance[nextNode]));
                 }
             }
+            
         }
-
-        return dist;
+        return distance;
     }
-}
+    public int[] dijkstra(int V, int[][] edges, int src) {
+        // code here
+        Map<Integer,List<Pair>> adj=buildGraph(edges,V);
+        
+        return findShortestDistamce(V,adj,src);
+        
+    }
+    
